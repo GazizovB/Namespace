62dc7e20198ef3a3e9da9bac6905970a973ae36b
Бауыржан <@->
2019-01-07 02:04:22 +0600

    Добавьте файлы проекта.

---
+++ C_Sharp_Homework/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Person = C_Sharp_Homework.User;
+using Printer = System.Console;
+
+namespace C_Sharp_Homework
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.name = "Lesson";
+            Printer.WriteLine(person.name);
+
+            Console.ReadLine();
+        }
+    }
+
+    class User
+    {
+        public string name;
+    }
+}
+
+
